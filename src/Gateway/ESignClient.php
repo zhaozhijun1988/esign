387acc089a60cc46c928af60c232152fd6453763
Zhijun Zhao <zhaozhijun1988@gmail.com>
2020-06-25 11:06:33 +0800

    fix find sign url

--- src/Gateway/ESignClient.php
+++ src/Gateway/ESignClient.php
@@ -236,11 +236,12 @@ class ESignClient
         return $response === null;
     }
 
-    public function findSignUrl(string $flowId, string $accountId, string  $appScheme, int $urlType = 0)
+    public function findSignUrl(string $flowId, string $accountId,  string $orgId, string  $appScheme, int $urlType = 0)
     {
         $response = $this->request('get', sprintf("/v1/signflows/%s/executeUrl", $flowId), [
             'query' => [
                 'accountId' => $accountId,
+                'organizeId' => $orgId,
                 'urlType' => $urlType,
                 'appScheme' => $appScheme
             ]
